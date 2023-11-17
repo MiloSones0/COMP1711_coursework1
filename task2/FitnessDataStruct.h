e0c13c911d32b864fea3fb70962636ee493abcfb
Milo Sones <146327226+MiloSones0@users.noreply.github.com>
2023-11-17 17:28:24 +0000

    open_file ow works

--- task2/FitnessDataStruct.h
+++ task2/FitnessDataStruct.h
@@ -1,7 +1,8 @@
 #ifndef FITNESS_DATA_STRUCT_H
 #define FITNESS_DATA_STRUCT_H
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #define buffer_size 100
 #define dataArray_size 500
 
@@ -38,25 +39,50 @@ void tokeniseRecord(const char *input, const char *delimiter, char *date, char *
 
 }
 
-int total_records(FILE * file);
+
+
+
+// int total_records(FITNESS_DATA *data) {
+//     // add error checking for file probably in seperate function.
+//     // change to use open_file func at some point
+//     char line_buffer[buffer_size];
+//     int total_records = 0;
+//     for (int i = 0; i < dataArray_size; i++){
+//         continue;
+//     }
+//     return total_records;
+// }
 
 int menu(FILE * file);
 
 int fewest_steps(FILE * file);
 
-FILE *open_file() {
+
+/**
+ * @brief Prompts the user for a file name, opens said file and save to an array
+ * @param fitnessDataArray The array that is updated with data inside file
+ * @returns number of lines in the file as int or 0 if error opening file
+ */
+
+int open_file(FITNESS_DATA * fitnessDataArray) {
     char filename[50];
-    printf("Input filename:");
+	char line[buffer_size];
+	int linesCounter;
+    printf("Input filename: ");
     scanf("%s", filename); 
     FILE *file = fopen(filename, "r");
     if (file == NULL)
     {
-        // change to repromt user and also add checking data is correct format
-        printf("Error opening file\n");
-        printf("Make sure filename is correct\n");
+        printf("Error: Could not find or open the file.\n");
         return 0;
     }
-    return file;
+	while (fgets(line, buffer_size, file)){
+		char steps[8];
+		tokeniseRecord(line, ",", fitnessDataArray[linesCounter].date, fitnessDataArray[linesCounter].time, steps);
+		fitnessDataArray[linesCounter].steps = atoi(steps);
+		linesCounter ++;
+	}
+    return linesCounter;
 }
 
 #endif // FITNESS_DATA_STRUCT_H
--- task2/StepCounter.c
+++ task2/StepCounter.c
@@ -3,24 +3,12 @@
 #include <string.h>
 #include "FitnessDataStruct.h"
 
-// Struct moved to header file
-
-// Define any additional variables here
-
-// Global variables for filename and FITNESS_DATA array
-
-
-// This is your helper function. Do not change it in any way.
-// Inputs: character array representing a row; the delimiter character
-// Ouputs: date character array; time character array; steps character array
-
-int main(){
-    // FILE * file;
-    // while(menu(file));
+int main() {
     FITNESS_DATA data[dataArray_size];
     char input;
+    int lines;
     while(1) {
-        printf( "Menu Options:"
+        printf( "Menu Options:\n"
                 "A: Specify the filename to be imported\n"
                 "B: Display the total number of records in the file\n"
                 "C: Find the date and time of the timeslot with the fewest steps\n"
@@ -29,114 +17,50 @@ int main(){
                 "F: Find the longest continuous period where the step count is above 500 steps\n"
                 "Q: Quit\n");
         input = getchar();
-    }
-    while (getchar() != '\n');
-    switch (input)
-    {
-    case 'A':
-    case 'a':
-        return 0;
-        break;
-    case 'B':
-    case 'b':
-        return 0;
-        break;
+        while (getchar() != '\n');
+        switch (input)
+        {
+        case 'A':
+        case 'a':; // weird not allowed to define a variable as the first thing you do so have to have a semi colon above.
+            lines = open_file(data);
+            if (lines == 0){
+                exit(1);
+            }
+            break;
+        case 'B':
+        case 'b':
+            printf("B");
+            break;
 
-    case 'C':
-    case 'c':
-        return 0;
-        break;
+        case 'C':
+        case 'c':
+            printf("C");
+            break;
 
-    case 'D':
-    case 'd':
-        return 0;
-        break;
+        case 'D':
+        case 'd':
+            printf("D");
+            break;
 
-    case 'E':
-    case 'e':
-        return 0;
-        break;
+        case 'E':
+        case 'e':
+            printf("E");
+            break;
 
-    case 'F':
-    case 'f':
-        return 0;
-        break;
+        case 'F':
+        case 'f':
+            printf("F");
+            break;
+        case 'Q':
+        case 'q':
+            printf("Q");
+            exit(0);
+            break;
 
-    case 'G':
-    case 'g':
-        return 0;
-        break;
-
-    case 'Q':
-    case 'q':
-        return 0;
-        break;
-
-    // if they type anything else:
-    default:
-        printf("Invalid choice. Try again.\n");
+        // if they type anything else:
+        default:
+            printf("Invalid choice. Try again.\n");
+        }
     }
 
-
-}
-
-
-int total_records(FITNESS_DATA *data) {
-    // add error checking for file probably in seperate function.
-    // change to use open_file func at some point
-    char line_buffer[buffer_size];
-    int total_records = 0;
-    for (int i = 0; i < dataArray_size; i++){
-        
-    }
-    return total_records;
-}
-
-int fewest_steps(FILE * file) {
-    return 0;
-}
-
-
-
-
-int menu(FILE * file) {
-    char input[1];
-    printf( "Menu Options:"
-            "A: Specify the filename to be imported\n"
-            "B: Display the total number of records in the file\n"
-            "C: Find the date and time of the timeslot with the fewest steps\n"
-            "D: Find the data and time of the timeslot with the largest number of steps\n"
-            "E: Find the mean step count of all the records in the file\n"
-            "F: Find the longest continuous period where the step count is above 500 steps\n"
-            "Q: Quit\n");
-    // Add check that a file has been provided and is valid before allowing any other function to run
-    // have a valid bool that is default value false and is updated when open_file is called and check in each if breanch.
-    scanf("%s", input);
-    if(!strcmp(input, "A")) {
-        file = open_file();
-    }
-    else if(!strcmp(input, "B")) {
-        printf("Total records: %d", total_records(file));
-
-    }
-    else if(!strcmp(input, "C")) {
-        printf("Fewest steps: ");
-    }
-    else if(!strcmp(input, "D")) {
-        printf("Largest steps: ");
-    }
-    else if(!strcmp(input, "E")) {
-        printf("Mean step count: ");
-    }
-    else if(!strcmp(input, "F")) {
-        printf("Longest period start: \n"
-               "Longest period end: ");
-    }
-    else if(!strcmp(input, "Q")) {
-        return 0;
-    }
-    else {
-        printf("Invalid input\n");
-    }
-    return 1;
 }
